a2fb9dcd5224c351e29baa0a93eae840546556e6
amey09 <ameymhatre09@gmail.com>
2023-11-04 00:19:36 +0530

    feat: add smooth scrolling to sections
    This commit adds id's to each section and enables smooth-scrolling: true applied on html in index.css
    
    change Name of DiscountCard.jsx to Services.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,113 +1,120 @@
-import React from 'react'
-import {Box, Button, Flex, Heading, Image, Text} from "@chakra-ui/react";
+import React from "react";
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 function About() {
-    return (
-        <Flex
-            flexDir={"column"}
-            paddingBlock={"5rem"}
-            maxW={"85vw"}
-            marginInline={"auto"}
-            as={"section"}
-            gap={"3.5rem"}
-        >
-            <Flex flexDir={{ base: "column", md: "row" }}>
-                {/* Left */}
-                <Flex gap={"clamp(1.2rem, 2vw, 2rem)"} flex={1}>
-                    {/* InnerLeft */}
-                    <Flex flexDir={"column"} gap={"clamp(1.2rem, 2vw, 2rem)"} flex={1}>
-                        <Flex>
-                            <Image
-                                src="./grid/4.png"
-                                width={"100%"}
-                                height={"100%"}
-                                objectFit={"contain"}
-                            />
-                        </Flex>
-                        <Flex gap={"clamp(10px, 2vw, 2rem)"} flex={1}>
-                            <Box flex={1}>
-                                <Image
-                                    src="./grid/1.png"
-                                    marginBottom={"auto"}
-                                    height={"85%"}
-                                    width={"100%"}
-                                    objectFit={"fill"}
-                                />
-                            </Box>
-                            <Box flex={1}>
-                                <Image
-                                    src="./grid/2.png"
-                                    width={"100%"}
-                                    height={"100%"}
-                                    objectFit={"fill"}
-                                />
-                            </Box>
-                        </Flex>
-                    </Flex>
-
-                    {/* InnerRight */}
-                    <Box flex={1}>
-                        <Image
-                            src="./grid/3.png"
-                            width={"100%"}
-                            height={"100%"}
-                            objectFit={"cover"}
-                        />
-                    </Box>
-                </Flex>
-
-                {/* Right */}
-                <Flex
-                    flexDir={"column"}
-                    justifyContent={"flex-start"}
-                    flex={1}
-                    color={"white"}
-                    paddingLeft={{ base: "0", md: "4rem" }}
-                    paddingTop={{ base: "1.5rem", md: 0 }}
-                >
-                    <Heading fontFamily={"jost"} fontWeight={"700"} color={"#FBB034"} alignSelf={"center"} fontSize={"clamp(1.5rem, 3vw, 6rem)"}>
-                        About us
-                    </Heading>
-                    <Heading
-                        marginTop={"5rem"}
-                        fontFamily={"pop"}
-                        fontWeight={"700"}
-                        fontSize={"clamp(1.5rem, 2.5vw, 6rem)"}
-                    >
-                        Best Haircut Salon
-                        <br />
-                        For Men
-                    </Heading>
-                    <Text
-                        marginTop={"1rem"}
-                        fontFamily={"jost"}
-                        fontWeight={"400"}
-                        fontSize={"clamp(1.2rem, 2vw, 2.5rem)"}
-                        lineHeight={{ base: "short", sm: "normal" }}
-                    >
-                        Let your hairdressers do their amazing job. Trim the hair to get
-                        your desired look. Book appointment with us for your favorite hair
-                        Styles!
-                    </Text>
-                </Flex>
+  return (
+    <Flex
+      id={"about"}
+      flexDir={"column"}
+      paddingBlock={"5rem"}
+      maxW={"85vw"}
+      marginInline={"auto"}
+      as={"section"}
+      gap={"3.5rem"}
+    >
+      <Flex flexDir={{ base: "column", md: "row" }}>
+        {/* Left */}
+        <Flex gap={"clamp(1.2rem, 2vw, 2rem)"} flex={1}>
+          {/* InnerLeft */}
+          <Flex flexDir={"column"} gap={"clamp(1.2rem, 2vw, 2rem)"} flex={1}>
+            <Flex>
+              <Image
+                src="./grid/4.png"
+                width={"100%"}
+                height={"100%"}
+                objectFit={"contain"}
+              />
             </Flex>
-            <Button
-                colorScheme="yellow"
-                bgColor="#FBB034"
-                borderRadius={"15px"}
-                maxW={"fit-content"}
-                alignSelf={"center"}
-                fontFamily={"pop"}
-                fontWeight={"700"}
-                lineHeight={"16px"}
-                paddingInline={"clamp(2.35rem, 3vw, 10rem)"}
-                paddingBlock={"clamp(0.5rem, 1.6vw, 2rem)"}
-                fontSize={"clamp(0.9rem, 1.2vw, 4rem)"}
-            >
-                Read More
-            </Button>
+            <Flex gap={"clamp(10px, 2vw, 2rem)"} flex={1}>
+              <Box flex={1}>
+                <Image
+                  src="./grid/1.png"
+                  marginBottom={"auto"}
+                  height={"85%"}
+                  width={"100%"}
+                  objectFit={"fill"}
+                />
+              </Box>
+              <Box flex={1}>
+                <Image
+                  src="./grid/2.png"
+                  width={"100%"}
+                  height={"100%"}
+                  objectFit={"fill"}
+                />
+              </Box>
+            </Flex>
+          </Flex>
+
+          {/* InnerRight */}
+          <Box flex={1}>
+            <Image
+              src="./grid/3.png"
+              width={"100%"}
+              height={"100%"}
+              objectFit={"cover"}
+            />
+          </Box>
         </Flex>
-    )
+
+        {/* Right */}
+        <Flex
+          flexDir={"column"}
+          justifyContent={"flex-start"}
+          flex={1}
+          color={"white"}
+          paddingLeft={{ base: "0", md: "4rem" }}
+          paddingTop={{ base: "1.5rem", md: 0 }}
+        >
+          <Heading
+            fontFamily={"jost"}
+            fontWeight={"700"}
+            color={"#FBB034"}
+            alignSelf={"center"}
+            fontSize={"clamp(1.5rem, 3vw, 6rem)"}
+          >
+            About us
+          </Heading>
+          <Heading
+            marginTop={"5rem"}
+            fontFamily={"pop"}
+            fontWeight={"700"}
+            fontSize={"clamp(1.5rem, 2.5vw, 6rem)"}
+          >
+            Best Haircut Salon
+            <br />
+            For Men
+          </Heading>
+          <Text
+            marginTop={"1rem"}
+            fontFamily={"jost"}
+            fontWeight={"400"}
+            fontSize={"clamp(1.2rem, 2vw, 2.5rem)"}
+            lineHeight={{ base: "short", sm: "normal" }}
+          >
+            Let your hairdressers do their amazing job. Trim the hair to get
+            your desired look. Book appointment with us for your favorite hair
+            Styles!
+          </Text>
+        </Flex>
+      </Flex>
+      <Button
+        colorScheme="yellow"
+        bgColor="#FBB034"
+        borderRadius={"15px"}
+        maxW={"fit-content"}
+        alignSelf={"center"}
+        fontFamily={"pop"}
+        fontWeight={"700"}
+        lineHeight={"16px"}
+        paddingInline={"clamp(2.35rem, 3vw, 10rem)"}
+        paddingBlock={"clamp(0.5rem, 1.6vw, 2rem)"}
+        fontSize={"clamp(0.9rem, 1.2vw, 4rem)"}
+      >
+        Read More
+      </Button>
+    </Flex>
+  );
 }
 
-export default About
+export default About;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,153 +1,160 @@
-import React from 'react'
-import {Flex, Input, InputGroup, InputRightElement, Text} from "@chakra-ui/react";
-import {ChevronRightIcon} from "@chakra-ui/icons";
+import React from "react";
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 function Footer() {
-    return (
-        <Flex
-            as={"footer"}
-            color={"white"}
-            gap={"5rem"}
-            maxW={"85vw"}
-            marginInline={"auto"}
-            paddingBlock={"5rem"}
-            flexDir={{ base: "column", md: "row" }}
+  return (
+    <Flex
+    id="footer"
+      as={"footer"}
+      color={"white"}
+      gap={"5rem"}
+      maxW={"85vw"}
+      marginInline={"auto"}
+      paddingBlock={"5rem"}
+      flexDir={{ base: "column", md: "row" }}
+    >
+      {/* Column 1 */}
+      <Flex flexDir={"column"} flex={"30%"} maxW={{ base: "100%", sm: "60%" }}>
+        <Text
+          fontFamily={"dmsans"}
+          fontWeight={"400"}
+          fontSize={"clamp(1rem, 2vw, 3rem)"}
+          flex={1}
         >
-            {/* Column 1 */}
-            <Flex
-                flexDir={"column"}
-                flex={"30%"}
-                maxW={{ base: "100%", sm: "60%" }}
+          Subscribe to our
+          <br />
+          newsletter
+        </Text>
+        <InputGroup marginTop={{ base: "1rem", sm: "0" }}>
+          <InputRightElement
+            bgColor={"#FBB034"}
+            alignItems={"center"}
+            borderRadius={"8px 8px 0px 0px"}
+          >
+            <ChevronRightIcon fontSize={"clamp(1rem, 1.5vw, 3rem)"} />
+          </InputRightElement>
+          <Input
+            placeholder="Email address"
+            variant={"flushed"}
+            _placeholder={{ color: "#8b8b8a" }}
+            fontSize={"clamp(1rem, 1.3vw, 3rem)"}
+            borderBottomColor={"#292927"}
+          />
+        </InputGroup>
+      </Flex>
+
+      {/* Column 2 */}
+      <Flex
+        flex={"70%"}
+        justifyContent={"space-between"}
+        gap={{ base: "2rem", sm: 0 }}
+        flexWrap={{ base: "wrap", md: "none" }}
+        whiteSpace={"nowrap"}
+        textAlign={"right"}
+      >
+        <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
+          <ul
+            className="footer-list"
+            style={{
+              listStyle: "none",
+              gap: "0.8rem",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text
+              as={"ul"}
+              color={"#FBB034"}
+              fontFamily={"dmsans"}
+              fontSize={"clamp(1rem, 2vw, 3rem)"}
+              paddingBottom={"0.5rem"}
             >
-                <Text fontFamily={"dmsans"} fontWeight={"400"} fontSize={"clamp(1rem, 2vw, 3rem)"} flex={1}>
-                    Subscribe to our
-                    <br />
-                    newsletter
-                </Text>
-                <InputGroup marginTop={{ base: "1rem", sm: "0" }}>
-                    <InputRightElement
-                        bgColor={"#FBB034"}
-                        alignItems={"center"}
-                        borderRadius={"8px 8px 0px 0px"}
-
-                    >
-                        <ChevronRightIcon fontSize={"clamp(1rem, 1.5vw, 3rem)"} />
-                    </InputRightElement>
-                    <Input
-                        placeholder="Email address"
-                        variant={"flushed"}
-                        _placeholder={{ color: "#8b8b8a" }}
-                        fontSize={"clamp(1rem, 1.3vw, 3rem)"}
-                        borderBottomColor={"#292927"}
-                    />
-                </InputGroup>
-            </Flex>
-
-            {/* Column 2 */}
-            <Flex
-                flex={"70%"}
-                justifyContent={"space-between"}
-                gap={{ base: "2rem", sm: 0 }}
-                flexWrap={{ base: "wrap", md: "none" }}
-                whiteSpace={"nowrap"}
-                textAlign={"right"}
-            >
-                <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
-                    <ul
-                        className="footer-list"
-                        style={{
-                            listStyle: "none",
-                            gap: "0.8rem",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Text
-                            as={"ul"}
-                            color={"#FBB034"}
-                            fontFamily={"dmsans"}
-                            fontSize={"clamp(1rem, 2vw, 3rem)"}
-                            paddingBottom={"0.5rem"}
-                        >
-                            Services
-                        </Text>
-                        <li>
-                            <a href="/#">Email Marketing</a>
-                        </li>
-                        <li>
-                            <a href="/#">Campaigns</a>
-                        </li>
-                        <li>
-                            <a href="/#">Brandings</a>
-                        </li>
-                        <li>
-                            <a href="/#">Offline</a>
-                        </li>
-                    </ul>
-                </Flex>
-
-                <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
-                    <ul
-                        style={{
-                            listStyle: "none",
-                            gap: "0.8rem",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Text
-                            as={"ul"}
-                            color={"#FBB034"}
-                            fontFamily={"dmsans"}
-                            fontSize={"clamp(1rem, 2vw, 3rem)"}
-                            paddingBottom={"0.5rem"}
-                        >
-                            About
-                        </Text>
-                        <li>
-                            <a href="/#">Our Story</a>
-                        </li>
-                        <li>
-                            <a href="/#">Benefits</a>
-                        </li>
-                        <li>
-                            <a href="/#">Team</a>
-                        </li>
-                        <li>
-                            <a href="/#">Careers</a>
-                        </li>
-                    </ul>
-                </Flex>
-
-                <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
-                    <ul
-                        style={{
-                            listStyle: "none",
-                            gap: "0.8rem",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Text
-                            as={"ul"}
-                            color={"#FBB034"}
-                            fontFamily={"dmsans"}
-                            fontSize={"clamp(1rem, 2vw, 3rem)"}
-                            paddingBottom={"0.5rem"}
-                        >
-                            Help
-                        </Text>
-                        <li>
-                            <a href="/#">FAQs</a>
-                        </li>
-                        <li>
-                            <a href="/#">Contact Us</a>
-                        </li>
-                    </ul>
-                </Flex>
-            </Flex>
+              Services
+            </Text>
+            <li>
+              <a href="/#">Email Marketing</a>
+            </li>
+            <li>
+              <a href="/#">Campaigns</a>
+            </li>
+            <li>
+              <a href="/#">Brandings</a>
+            </li>
+            <li>
+              <a href="/#">Offline</a>
+            </li>
+          </ul>
         </Flex>
-    )
+
+        <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
+          <ul
+            style={{
+              listStyle: "none",
+              gap: "0.8rem",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text
+              as={"ul"}
+              color={"#FBB034"}
+              fontFamily={"dmsans"}
+              fontSize={"clamp(1rem, 2vw, 3rem)"}
+              paddingBottom={"0.5rem"}
+            >
+              About
+            </Text>
+            <li>
+              <a href="/#">Our Story</a>
+            </li>
+            <li>
+              <a href="/#">Benefits</a>
+            </li>
+            <li>
+              <a href="/#">Team</a>
+            </li>
+            <li>
+              <a href="/#">Careers</a>
+            </li>
+          </ul>
+        </Flex>
+
+        <Flex flexDir={"column"} flex={{ base: 1, md: 0 }}>
+          <ul
+            style={{
+              listStyle: "none",
+              gap: "0.8rem",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text
+              as={"ul"}
+              color={"#FBB034"}
+              fontFamily={"dmsans"}
+              fontSize={"clamp(1rem, 2vw, 3rem)"}
+              paddingBottom={"0.5rem"}
+            >
+              Help
+            </Text>
+            <li>
+              <a href="/#">FAQs</a>
+            </li>
+            <li>
+              <a href="/#">Contact Us</a>
+            </li>
+          </ul>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
 
-export default Footer
+export default Footer;
